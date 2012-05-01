c4227e5c7849bf3ef16f4129758a225ee487f003
Vincent Rabaud <vrabaud@willowgarage.com>
2012-05-01 15:37:26 -0700

    rename the stack to object_recognition_linemod

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 from distutils.core import setup
 
-setup(name='LINE-MOD',
+setup(name='object_recognition_linemod',
       version='1.0.0',
       description='A pipeline for object recognition using LINE-MOD from OpenCV',
-      packages=['linemod'],
+      packages=['object_recognition_linemod'],
       package_dir={'':'python'}
 )
