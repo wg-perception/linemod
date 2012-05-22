86198ea04bb27f0f4d7f8bc64ed71f3f0db5b679
Vincent Rabaud <vrabaud@willowgarage.com>
2012-05-22 15:25:25 -0700

    comply to the new DB API

--- python/object_recognition_linemod/detector.py
+++ python/object_recognition_linemod/detector.py
@@ -3,7 +3,7 @@
 Module defining the LINE-MOD detector to find objects in a scene
 """
 
-from object_recognition_core.db import ObjectDb, DbModels
+from object_recognition_core.db import ObjectDb, Models
 from object_recognition_core.utils import json_helper
 from object_recognition_core.pipelines.detection import DetectionPipeline
 import ecto_cells.ecto_linemod as ecto_linemod
@@ -21,6 +21,6 @@ class LinemodDetectionPipeline(DetectionPipeline):
         parameters = kwargs.get('parameters')
         object_ids = parameters['object_ids']
         object_db = ObjectDb(parameters['db'])
-        model_documents = DbModels(object_db, object_ids, self.type_name(), json_helper.dict_to_cpp_json_str(submethod))
+        model_documents = Models(object_db, object_ids, self.type_name(), json_helper.dict_to_cpp_json_str(submethod))
         threshold = parameters.get('threshold', 90)
         return ecto_linemod.Detector(model_documents=model_documents, db=object_db, threshold=threshold)
