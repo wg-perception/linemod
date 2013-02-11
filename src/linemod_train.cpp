9eb4aaa38424c13027b00845125c51477cfaf780
Vincent Rabaud <vrabaud@willowgarage.com>
2013-02-11 21:30:17 +0100

    comply to the new upsteam API and load the mesh from the DB and not from a file

--- src/linemod_train.cpp
+++ src/linemod_train.cpp
@@ -34,6 +34,8 @@
  */
 
 #include <iostream>
+#include <fstream>
+#include <stdio.h>
 #include <vector>
 
 #include <ecto/ecto.hpp>
@@ -44,6 +46,8 @@
 #include <opencv2/objdetect/objdetect.hpp>
 
 #include <object_recognition_core/common/json.hpp>
+#include <object_recognition_core/db/db.h>
+#include <object_recognition_core/db/model_utils.h>
 #include <object_recognition_renderer/renderer_osmesa.h>
 
 using ecto::tendrils;
@@ -54,16 +58,13 @@ namespace ecto_linemod
   struct Trainer
   {
     static void
-    declare_params(tendrils& params)
-    {
-      /// @todo Parameters for various LINE-MOD settings?
-      params.declare(&Trainer::object_id_, "object_id", "The path to where the mesh to generate templates from is.").required(
-          true);
-    }
-
-    static void
     declare_io(const tendrils& params, tendrils& inputs, tendrils& outputs)
     {
+    inputs.declare(&Trainer::json_db_, "json_db", "The DB parameters", "{}").required(
+        true);
+    inputs.declare(&Trainer::object_id_, "object_id",
+        "The object id, to associate this model with.").required(true);
+
       outputs.declare(&Trainer::detector_, "detector", "The LINE-MOD detector");
       outputs.declare(&Trainer::Rs_, "Rs", "The matching rotations of the templates");
       outputs.declare(&Trainer::Ts_, "Ts", "The matching translations of the templates.");
@@ -75,9 +76,35 @@ namespace ecto_linemod
       //or_json::mValue submethod = object_recognition_core::to_json(*json_submethod_);
     }
 
-    int
-    process(const tendrils& inputs, const tendrils& outputs)
+  int process(const tendrils& inputs, const tendrils& outputs) {
+    // Get the document for the object_id_ from the DB
+    object_recognition_core::db::ObjectDbPtr db =
+        object_recognition_core::db::ObjectDbParameters(*json_db_).generateDb();
+    object_recognition_core::db::Documents documents =
+        object_recognition_core::db::ModelDocuments(db,
+            std::vector<object_recognition_core::db::ObjectId>(1, *object_id_),
+            "mesh", "");
+    if (documents.empty()) {
+      std::stringstream ss;
+      ss << "No object with id \"" << *object_id_ << "\" with a mesh in the DB"
+          << std::endl;
+      throw std::runtime_error(ss.str());
+    }
+
+    // Load the mesh and save it to a temporary file
+    object_recognition_core::db::Document document = documents[0];
+    std::string mesh_path;
     {
+      char mesh_path_tmp[L_tmpnam];
+      tmpnam(mesh_path_tmp);
+      mesh_path = std::string(mesh_path_tmp) + ".stl";
+    }
+
+    std::ofstream mesh_file;
+    mesh_file.open(mesh_path.c_str());
+    document.get_attachment_stream("mesh.stl", mesh_file);
+    mesh_file.close();
+
       cv::Ptr<cv::linemod::Detector> detector_ptr = cv::linemod::getDefaultLINEMOD();
       *detector_ = *detector_ptr;
 
@@ -86,13 +113,11 @@ namespace ecto_linemod
       double near = 0.1, far = 1000;
       double focal_length_x = 525, focal_length_y = 525;
 
-      // TODO, load the mesh from the DB
-
-      // the model name can be specified on the command line.
-      std::string path;
-      RendererOSMesa renderer = RendererOSMesa(path);
-
-      renderer.set_parameters(width, height, focal_length_x, focal_length_y, near, far);
+    // the model name can be specified on the command line.
+    RendererOSMesa renderer = RendererOSMesa(mesh_path);
+    renderer.set_parameters(width, height, focal_length_x, focal_length_y, near,
+        far);
+    std::remove(mesh_path.c_str());
 
       RendererIterator renderer_iterator = RendererIterator(&renderer, 150);
 
@@ -102,7 +127,10 @@ namespace ecto_linemod
       cv::Vec3d T;
       for (size_t i = 0; !renderer_iterator.isDone(); ++i, ++renderer_iterator)
       {
-        std::cout << "Loading images " << i << std::endl;
+      std::stringstream status;
+      status << "Loading images " << (i+1) << "/"
+          << renderer_iterator.n_templates();
+      std::cout << status.str();
 
         renderer_iterator.render(image, depth, mask);
         R = renderer_iterator.R();
@@ -119,12 +147,19 @@ namespace ecto_linemod
         // Also store the pose of each template
         Rs_->push_back(cv::Mat(R));
         Ts_->push_back(cv::Mat(T));
-      }
+
+      // Delete the status
+      for (size_t j = 0; j < status.str().size(); ++j)
+        std::cout << '\b';
+    }
 
       return ecto::OK;
     }
 
-    spore<std::string> object_id_;
+    /** The DB parameters as a JSON string */
+    ecto::spore<std::string> json_db_;
+    /** The id of the object to generate a trainer for */
+    ecto::spore<std::string> object_id_;
     ecto::spore<cv::linemod::Detector> detector_;
     spore<std::vector<cv::Mat> > Rs_;
     spore<std::vector<cv::Mat> > Ts_;
